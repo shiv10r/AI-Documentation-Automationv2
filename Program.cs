5e54bf74a9ad859dc984e1d05da7398678abc6bf
shr <shr1030cd@gmail.com>
2026-01-01 19:25:15 +0530

    cors

--- Program.cs
+++ Program.cs
@@ -8,20 +8,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Register DI
+// Enable CORS for React frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // React app URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// Register DI for services and repositories
 builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
 builder.Services.AddScoped<IDocumentService, DocumentService>();
 
 var app = builder.Build();
 
-// Swagger
+// Use Swagger in development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Enable CORS
+app.UseCors("AllowReactApp");
+
+// HTTPS redirection and Authorization
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
+// Map controllers
 app.MapControllers();
+
+// Run the app
 app.Run();
